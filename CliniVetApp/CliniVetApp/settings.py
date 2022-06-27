91fdfcdb2a42123e712ff813869327618d9d8835
Fernando Marques Soares <fernando.soares.trabalho@gmail.com>
2022-06-27 06:24:40 -0300

    criado categories

--- CliniVetApp/CliniVetApp/settings.py
+++ CliniVetApp/CliniVetApp/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'core.apps.CoreConfig', 
+    'categories.apps.CategoriesConfig',
 ]
 
 MIDDLEWARE = [
